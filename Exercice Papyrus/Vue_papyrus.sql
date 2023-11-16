317e52ed1a58587c7e3e19e648d386e59be19a2f
KaidoOwls <Seydina.diallo60@gmail.com>
2023-11-16 15:52:35 +0100

    fin des procédures stockés

--- Exercice Papyrus/Vue_papyrus.sql
+++ Exercice Papyrus/Vue_papyrus.sql
@@ -1,1 +1,21 @@
 CALL Lst_Commandes('%cad%');
+DELIMITER //
+
+CREATE PROCEDURE CA_Fournisseur(
+    IN p_numfou INT,
+    IN p_annee INT
+)
+BEGIN
+    SELECT
+        SUM(ligcom.qtecde * ligcom.priuni) AS CAPotentiel
+    FROM
+        entcom
+            JOIN ligcom ON entcom.numcom = ligcom.numcom
+    WHERE
+            entcom.numfou = p_numfou
+      AND YEAR(entcom.datcom) = p_annee;
+END //
+
+DELIMITER ;
+
+CALL CA_Fournisseur(120, 2022);
